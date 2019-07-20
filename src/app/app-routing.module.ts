bcdeb456739d73341a8fc328142c4c47acecbad7
Deanne Alabastro <aladeanra@gmail.com>
2019-07-20 15:35:50 +1200

    create password reset page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { AuthGuard } from './services/user/auth.guard';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
 ];
 
 @NgModule({
