d716bd50aa7093c6ed18d4a6f0dba09515b01b73
Deanne Alabastro <aladeanra@gmail.com>
2019-07-24 20:12:37 +1200

    Used button implementation following datepicker docs here: https://www.logisticinfotech.com/blog/ionic4-datepicker-component/

--- src/app/components/calendar-date-picker/calendar-date-picker.component.ts
+++ src/app/components/calendar-date-picker/calendar-date-picker.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Ionic4DatepickerModalComponent } from '@logisticinfotech/ionic4-datepicker';
 
 @Component({
   selector: 'app-calendar-date-picker',
@@ -6,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar-date-picker.component.scss'],
 })
 export class CalendarDatePickerComponent implements OnInit {
+  selectedDate: any;
 
-  constructor() { }
+  datePickerObj: any = {
+    closeOnSelect: true,
+    mondayFirst: true,
+    titleLabel: 'Select a Date',
+    weeksList: [
+      'Sun','Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'
+    ],
+    momentLocale: 'en-NZ'
+  }
+
+
+  constructor(public modalCtrl: ModalController) { }
+
+  async openDatePicker() {
+    const datePickerModal = await this.modalCtrl.create({
+      component: Ionic4DatepickerModalComponent,
+      cssClass: 'li-ionic4-datePicker',
+      componentProps: { 'objConfig': this.datePickerObj }
+  });
+  await datePickerModal.present();
+  datePickerModal.onDidDismiss()
+    .then((data) => {
+      // this.isModalOpen = false;
+      if (data == 'Invalid date') {
+        console.log('hello');
+      }
+      console.log(data);
+      this.selectedDate = data.data.date;
+    });
+}
 
   ngOnInit() {}
 
--- src/app/components/calendar-date-picker/calendar-date-picker.module.ts
+++ src/app/components/calendar-date-picker/calendar-date-picker.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 import { Ionic4DatepickerModule } from '@logisticinfotech/ionic4-datepicker';
 import { CalendarDatePickerComponent } from './calendar-date-picker.component';
 
@@ -7,6 +8,7 @@ import { CalendarDatePickerComponent } from './calendar-date-picker.component';
   declarations: [CalendarDatePickerComponent],
   imports: [
     CommonModule,
+    IonicModule,
     Ionic4DatepickerModule
   ],
   exports: [
