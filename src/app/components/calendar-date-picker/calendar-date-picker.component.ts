cb18179ebef446e520f4c5319dfaef509bd55c40
Deanne Alabastro <aladeanra@gmail.com>
2019-07-26 00:09:44 +1200

    Make current date default date if data passed to modal is undefined (because user opened modal without selecting anything)

--- src/app/components/calendar-date-picker/calendar-date-picker.component.ts
+++ src/app/components/calendar-date-picker/calendar-date-picker.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { Ionic4DatepickerModalComponent } from '@logisticinfotech/ionic4-datepicker';
 import * as moment from 'moment';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-calendar-date-picker',
@@ -25,8 +26,10 @@ export class CalendarDatePickerComponent implements OnInit {
     momentLocale: 'en-NZ'
   }
 
+  // isStartDate: boolean;
+  // public whichDate: boolean,
 
-  constructor(public modalCtrl: ModalController) { }
+  constructor( public modalCtrl: ModalController) { }
 
   async openDatePicker() {
     const datePickerModal = await this.modalCtrl.create({
@@ -37,19 +40,20 @@ export class CalendarDatePickerComponent implements OnInit {
   await datePickerModal.present();
   datePickerModal.onDidDismiss()
     .then((data) => {
-      // this.isModalOpen = false;
-      if (data.data.date == 'Invalid date') {
-        this.selectedDate = moment(new Date()).format('D MMMM YYYY');
-        console.log(this.selectedDate);
+      if (isNullOrUndefined(data.data) || data.data.date === 'Invalid date') {
+        if (isNullOrUndefined(this.selectedDate)) {
+          this.selectedDate = moment(new Date()).format('D MMMM YYYY');
+          console.log(this.selectedDate);
+        }
       } else {
         this.selectedDate = data.data.date;
         console.log(this.selectedDate);
-      }
-      
-      
+      }    
     });
-}
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    //this.isStartDate = this.whichDate;
+  }
 
 }
