615d4fb4c628593f6453b10b4b21c9595c3eec3e
Deanne Alabastro <aladeanra@gmail.com>
2019-07-27 00:29:13 +1200

    add constructor

--- src/app/pages/user-profile/user-profile.page.ts
+++ src/app/pages/user-profile/user-profile.page.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { AuthService } from '../../services/user/auth.service';
+import { ProfileService } from '../../services/user/profile.service';
+import { Router } from '@angular/router';
 
+/**
+ * Allows the user to view and update their profile details.
+ * Code based on tut from https://javebratt.com/ionic-firebase-tutorial-object/ 
+ */
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.page.html',
   styleUrls: ['./user-profile.page.scss'],
 })
 export class UserProfilePage implements OnInit {
+  public userProfile: any;
 
-  constructor() { }
+  constructor(
+    private alertCtrl: AlertController,
+    private authService: AuthService,
+    private profileService: ProfileService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.profileService
+    .getUserProfile()
+    .get()
+    .then( userProfileSnapshot => {
+      this.userProfile = userProfileSnapshot.data();
+    });
   }
 
 }
