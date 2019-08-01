ea5d3357e734b9e9896a1e5a5ae0e5076a78ba6a
Deanne Alabastro <aladeanra@gmail.com>
2019-08-01 15:25:31 +1200

    and create and edit functions

--- src/app/services/todo/list.service.ts
+++ src/app/services/todo/list.service.ts
@@ -1,27 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ProfileService } from '../user/profile.service';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 import 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
-  public userLists: firebase.firestore.CollectionReference;
+  public userListsRef: firebase.firestore.CollectionReference;
 
-  constructor(private profileService: ProfileService) {
-    this.userLists = this.profileService.userProfile.collection('user_lists');
-    
+  constructor() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.userListsRef = firebase
+          .firestore()
+          .collection(`/userProfile/${user.uid}/user_lists`);
+      }
+    });
   }
 
-  addList(){
-
+  createList(listTitle:string): Promise<firebase.firestore.DocumentReference> {
+    // create the list that user defined (created as a document in firestore)
+    return this.userListsRef.add({
+      title: listTitle,
+    })
   }
 
-  editListName(){
-
+  editListTitle(listId:string, newListTitle:string){
+    this.userListsRef.doc(listId).update({
+      title: newListTitle
+    })
   }
 
   deleteList() {
-
+    
   }
 }
