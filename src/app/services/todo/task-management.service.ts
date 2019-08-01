5a81f8c01c54a8bcdec7a684e3fad98c83c1ecbb
Deanne Alabastro <aladeanra@gmail.com>
2019-08-01 16:57:02 +1200

    add delete, add, and edit functionality in task management

--- src/app/services/todo/task-management.service.ts
+++ src/app/services/todo/task-management.service.ts
@@ -1,9 +1,83 @@
 import { Injectable } from '@angular/core';
-
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskManagementService {
+  public userListsRef: firebase.firestore.CollectionReference;
 
-  constructor() { }
+  constructor() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.userListsRef = firebase
+          .firestore()
+          .collection(`/userProfile/${user.uid}/user_lists`);
+      }
+    });
+  }
+
+  addTask(
+    listId: string,
+    taskName: string,
+    taskStartDate: string,
+    taskEndDate: number,
+    taskStartTime: number,
+    taskEndTime: number,
+    taskLocation: string,
+    taskDescription: string): Promise<firebase.firestore.DocumentReference> {
+    return this.userListsRef
+      .doc(listId)
+      .collection('task_list')
+      .add({
+        name: taskName,
+        location: taskLocation,
+        startDate: taskStartDate,
+        startTime: taskStartTime,
+        endDate: taskEndDate,
+        endTime: taskEndTime,
+        description: taskDescription
+      })
+  }
+
+  editTask(
+    taskId: string,
+    listId: string,
+    taskName: string,
+    taskStartDate: string,
+    taskEndDate: number,
+    taskStartTime: number,
+    taskEndTime: number,
+    taskLocation: string,
+    taskDescription: string) {
+    this.userListsRef
+      .doc(listId)
+      .collection('task_list')
+      .doc(taskId)
+      .update({
+        name: taskName,
+        location: taskLocation,
+        startDate: taskStartDate,
+        startTime: taskStartTime,
+        endDate: taskEndDate,
+        endTime: taskEndTime,
+        description: taskDescription
+      })
+      .catch(function (error) {
+        console.error("Error updating document: ", error);
+      });
+  }
+
+  deleteTask(taskId: string, listId: string) {
+    this.userListsRef
+      .doc(listId)
+      .collection('task_list')
+      .doc(taskId)
+      .delete()
+      .catch(function (error) {
+        console.error("Error removing document: ", error);
+      });
+  }
+
 }
