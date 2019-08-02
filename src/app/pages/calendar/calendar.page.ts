04edb7fdd1d8b9360b84af378284adbca783460e
Deanne Alabastro <aladeanra@gmail.com>
2019-08-02 16:40:49 +1200

    created default placeholder methods for calendar functions

--- src/app/pages/calendar/calendar.page.ts
+++ src/app/pages/calendar/calendar.page.ts
@@ -12,4 +12,29 @@ export class CalendarPage implements OnInit {
   ngOnInit() {
   }
 
+  onViewTitleChanged(title:string) {
+    console.log(title);
+  }
+
+  onEventSelected(event:any) {
+    console.log('Event selected:' + event.startTime + '-' + event.endTime + ',' + event.title);
+  }
+
+  onTimeSelected(ev:any) {
+    console.log('Selected time: ' + ev.selectedTime + ', hasEvents: ' +
+      (ev.events !== undefined && ev.events.length !== 0) + ', disabled: ' + ev.disabled);
+  }
+
+  onCurrentDateChanged(event: Date) {
+    console.log('current date change: ' + event);
+  }
+
+  onRangeChanged(ev:any) {
+    console.log('range changed: startTime: ' + ev.startTime + ', endTime: ' + ev.endTime);
+  }
+
+  today() {
+    this.calendar.currentDate = new Date();
+  }
+
 }
