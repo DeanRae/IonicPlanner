0dee1193583d6e818988cc5e00596cccbd581d66
Deanne Alabastro <aladeanra@gmail.com>
2019-08-10 16:29:47 +1200

    create page for tab menu

---
+++ src/app/pages/tab-menu/tab-menu.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TabMenuPage } from './tab-menu.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabMenuPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [TabMenuPage]
+})
+export class TabMenuPageModule {}
