50e3594532e2f5940df0213920e5dae031dc99a1
Deanne Alabastro <aladeanra@gmail.com>
2019-08-13 02:18:18 +1200

    Add toast, completion rate, allDay functionality, and list creation functionality

--- src/app/components/todo-item-details/todo-item-details.component.ts
+++ src/app/components/todo-item-details/todo-item-details.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ListManagementService } from 'src/app/services/todo/list-management.service';
-import { AlertController, NavParams, ModalController } from '@ionic/angular';
+import { AlertController, NavParams, ModalController, ToastController, LoadingController } from '@ionic/angular';
 import { ActivatedRoute } from '@angular/router';
 import { TaskManagementService } from 'src/app/services/todo/task-management.service';
 import { isNullOrUndefined } from 'util';
-import { FormGroup, Validators, FormBuilder, FormControl, RequiredValidator } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
 import * as moment from 'moment';
 import { Task } from 'src/app/interfaces/task';
+import { DateTimePickerComponent } from '../date-time-picker/date-time-picker.component';
+import { discardPeriodicTasks } from '@angular/core/testing';
 @Component({
   selector: 'app-todo-item-details',
   templateUrl: './todo-item-details.component.html',
@@ -21,8 +23,9 @@ export class TodoItemDetailsComponent implements OnInit {
   public location: string = '';
   public description: string = '';
   public allDay: boolean = false;
+  public completionRate: number = 0;
 
-  constructor(private listManagementService: ListManagementService, private taskManagementService: TaskManagementService, public alertController: AlertController, private modalController:ModalController, private formBuilder: FormBuilder, private navParams: NavParams) {
+  constructor(private listManagementService: ListManagementService, private taskManagementService: TaskManagementService, public alertController: AlertController, private modalController: ModalController, private formBuilder: FormBuilder, private navParams: NavParams, private toastController: ToastController) {
     // create form validation for user inputted date
     this.taskForm = this.formBuilder.group({
       startTime: [moment(new Date()).format("D MMMM YYYY h:mm a"), this.dateValidator("D MMMM YYYY h:mm a")],
@@ -64,46 +67,56 @@ export class TodoItemDetailsComponent implements OnInit {
           this.startTime.setValue(taskSnapshot.get("startTime"));
           this.endTime.setValue(taskSnapshot.get("endTime"));
           this.allDay = taskSnapshot.get("allDay");
+          this.completionRate = taskSnapshot.get("completionRate")*100;
         });
     }
   }
 
   ngOnInit() {
-    
+
   }
 
   public async saveTask(taskForm: FormGroup): Promise<void> {
     if (!taskForm.valid) {
       console.log('Form is not valid yet, current value:', taskForm.value);
     } else {
+      let startAsDate = new Date(this.startTime.value);
+      let UTCStart = new Date(Date.UTC(startAsDate.getUTCFullYear(), startAsDate.getUTCMonth(), startAsDate.getUTCDate()));
+      let endAsDate = new Date(this.endTime.value);
+      let UTCEnd = new Date(Date.UTC(endAsDate.getUTCFullYear(), endAsDate.getUTCMonth(), endAsDate.getUTCDate()));
       let task: Task = {
         title: this.title.value,
         listId: this.selectedList,
         location: this.location,
-        startTime: this.startTime.value,
-        endTime: this.endTime.value,
+        startTime: this.allDay ? UTCStart: this.startTime.value,
+        endTime: this.allDay ? UTCEnd: this.endTime.value,
         allDay: this.allDay,
         isCompleted: false,
         description: this.description,
         subTasks: this.subTasks,
+        completionRate: this.completionRate/100
       }
 
       if (this.currentTask) {
         // update the task
         try {
           await this.taskManagementService.editTask(task, this.currentTask.id);
-          console.log("Task: ", task.title, " Edited");
+          this.presentToast('The task: ' + task.title + ' has been successfully updated');
+          this.dismissModal();
         }
         catch (error) {
+          this.presentToast('Error updating task:  ' + task.title);
           console.error("Error updating task: ", error);
         }
       } else {
         // create new task
         this.taskManagementService.createTask(task)
           .then(() => {
-            console.log("Task: ", task.title, " Created")
+            this.presentToast('The task: ' + task.title + ' has been successfully created');
+            this.dismissModal();
           })
           .catch(error => {
+            this.presentToast('Error creating task ' + task.title);
             console.error("Error creating task: ", error);
           })
       }
@@ -175,9 +188,38 @@ export class TodoItemDetailsComponent implements OnInit {
     await alert.present();
   }
 
+  /**
+   * Deletes a subtask from the task's list of subtasks
+   * @param index 
+   */
   deleteSubTask(index: number) {
     this.subTasks.splice(index, 1);
-    console.log("deleted subtask");
+  }
+
+  async presentListCreatorAlert() {
+    const alert = await this.alertController.create({
+      subHeader: 'Create a new list',
+      inputs: [
+        {
+          type: 'text',
+          name: 'listName',
+          placeholder: 'Enter list name',
+        }
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.listManagementService.createList(data.listName)
+              .then(() => {
+                this.presentToast('List ' + data.listName + ' Created');
+              });
+          },
+        },
+      ],
+    });
+    await alert.present();
   }
 
   /**
@@ -186,4 +228,21 @@ export class TodoItemDetailsComponent implements OnInit {
   public dismissModal() {
     this.modalController.dismiss();
   }
+
+  /**
+   * Presents the toast used to tell the user that task has been either
+   * created or edited.
+   * @param message 
+   */
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 2000,
+      position: 'middle',
+      color: 'dark',
+      showCloseButton: true
+    });
+    toast.present();
+  }
+
 }
