a3bcb4b0f64a3d8391f5ca397b8c4115371aaad4
Deanne Alabastro <aladeanra@gmail.com>
2019-08-14 10:58:03 +1200

    resolve compilation issues with imports that no longer exist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ const routes: Routes = [
   { path: 'reset-password', loadChildren: './pages/reset-password/reset-password.module#ResetPasswordPageModule' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule', canActivate: [AuthGuard] },
   { path: 'signup', loadChildren: './pages/signup/signup.module#SignupPageModule' },
-  { path: 'component-test-page', loadChildren: './pages/component-test-page/component-test-page.module#ComponentTestPagePageModule' },
   { path: 'user-profile', loadChildren: './pages/user-profile/user-profile.module#UserProfilePageModule', canActivate: [AuthGuard] },
   { path: 'calendar', loadChildren: './pages/calendar/calendar.module#CalendarPageModule', canActivate: [AuthGuard] },
   { path: 'ionic-planner', loadChildren: './pages/tab-menu/tab-menu.module#TabMenuPageModule', canActivate: [AuthGuard] },
--- src/app/components/todo-item-details/todo-item-details.module.ts
+++ src/app/components/todo-item-details/todo-item-details.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TodoItemDetailsComponent } from './todo-item-details.component';
 import { IonicModule } from '@ionic/angular';
-import { CalendarDatePickerModule } from '../calendar-date-picker/calendar-date-picker.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TimePickerComponent } from '../time-picker/time-picker.component';
-import { TimePickerModule } from '../time-picker/time-picker.module';
 import { DateTimePickerModule } from '../date-time-picker/date-time-picker.module';
 
 @NgModule({
@@ -15,8 +12,6 @@ import { DateTimePickerModule } from '../date-time-picker/date-time-picker.modul
     IonicModule,
     FormsModule,
     ReactiveFormsModule,
-    TimePickerModule,
-    CalendarDatePickerModule,
     DateTimePickerModule
   ],
   entryComponents: [
